1370e34a84ca5e0b32787abbdb35828b7d4fec13
Piter722 <129344747+Piter722@users.noreply.github.com>
2024-05-14 13:57:44 -0500

    bicicletas  inventario

--- comprar.php
+++ comprar.php
@@ -17,7 +17,7 @@ require_once 'php/CreaBD.php';
 require_once("PHP/component.php");
 
     //el registro de esta compra
-    $database = new CreaBD('elponyweb','inventario','localhost','root','');
+    $database = new CreaBD('elponyweb','inventario','localhost','root','1123');
     if(isset($_POST['agregar']))
     {
        // print_r($_POST['product_id']);
