785d894c32960081c17af870c36dcf40954f3047
Piter722 <168684691+NestorJ911@users.noreply.github.com>
2024-05-18 17:35:37 -0500

    boton cliente

--- admin-tienda.php
+++ admin-tienda.php
@@ -10,6 +10,20 @@
         <img src="IMG/Logo.png" alt="" class="w-75">
     </div>
 </div>
+<!-- botonera adminstrador -->
+
+<div class="row align navbar-blue bg-light">
+            <div class="container-fluid">
+                <div class="col-md-6">
+
+                </div>
+                <a href="registro.php" class="navbar-brand">
+                    <strong>Registrar cliente</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
 
 
 <!-- importamos el footer -->
