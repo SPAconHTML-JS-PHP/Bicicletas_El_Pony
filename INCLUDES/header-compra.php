04a9a6fa76b1de8443a6a7bd3b719ee760c51113
alexcun17 <167655770+alexcun17@users.noreply.github.com>
2024-05-18 11:22:53 -0500

    cambio de color en el header

--- INCLUDES/header-compra.php
+++ INCLUDES/header-compra.php
@@ -1,35 +1,34 @@
-
-
 <header id="header">
     <div class="container">
-    <nav class="navbar navbar-expand navbar-dark fixed-top">
-        <a href="comprar.php" class="navbar-brand">
-            <h3 class="px-2">
-                <i class="fas fa-shopping-basket"></i>Mira las Bicis
-            </h3>
-        </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav container">
-            <a href="cart.php" class="nav-item nav-link active ml-auto">
-        <h5>
-            <i class="fas fa-shopping-cart d-inline"></i>productos
-            <?php 
-                if(isset($_SESSION['cart']))
-                {
-                    $count = count($_SESSION['cart']);
-                    echo "<span id=\"cart_count\" class=\"d-inline mr-2\">$count</span>";
-                }
-                else
-                {
-                    echo "<span id=\"cart_count\" class=\"d-inline mr-2\">0</span>";
-                }
-            ?>
-        </h5></a>
-        </div>
-    </div>
-    </nav>
+        <nav class="navbar navbar-expand navbar-dark fixed-top" style="background-color: #001f3f;">
+            <a href="comprar.php" class="navbar-brand">
+                <h3 class="px-2" style="color: #ffffff;">
+                    <i class="fas fa-shopping-basket"></i>Mira las Bicis
+                </h3>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav container">
+                    <a href="cart.php" class="nav-item nav-link active ml-auto">
+                        <h5>
+                            <i class="fas fa-shopping-cart d-inline"></i>productos
+                            <?php 
+                                if(isset($_SESSION['cart']))
+                                {
+                                    $count = count($_SESSION['cart']);
+                                    echo "<span id=\"cart_count\" class=\"d-inline mr-2\">$count</span>";
+                                }
+                                else
+                                {
+                                    echo "<span id=\"cart_count\" class=\"d-inline mr-2\">0</span>";
+                                }
+                            ?>
+                        </h5>
+                    </a>
+                </div>
+            </div>
+        </nav>
     </div>
 </header>
